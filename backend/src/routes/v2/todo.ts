49c329484e1520c26b986621468be9d440a7851f
Shiva Raghav <shivaraghav200701@gmail.com>
2026-04-11 00:41:14 -0500

    feat: added get todos end point

--- backend/src/routes/v2/todo.ts
+++ backend/src/routes/v2/todo.ts
@@ -6,6 +6,35 @@ import prisma from "../../db/index.js";
 
 const todoRouter = Router();
 
+todoRouter.get("/", requireLogin, async (req, res) => {
+  const userId = req.session.userId;
+
+  if (!userId) {
+    return res.status(401).json({
+      msg: "unauthorized",
+    });
+  }
+  try {
+    const todos = await prisma.todo.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        notifications: true,
+      },
+    });
+
+    return res.status(200).json({
+      todos,
+    });
+  } catch (error) {
+    console.error("Failed getting todos", error);
+    return res.status(500).json({
+      msg: "internal Server Error",
+    });
+  }
+});
+
 todoRouter.post("/", requireLogin, async (req, res) => {
   const { data, success, error } = CreateTodoSchema.safeParse(req.body);
 
@@ -33,29 +62,36 @@ todoRouter.post("/", requireLogin, async (req, res) => {
     isAllDay,
   } = data;
 
-  const last = await prisma.todo.findFirst({
-    where: { userId, dueOn: completeAt },
-    orderBy: { sortKey: "asc" },
-    select: { sortKey: true },
-  });
+  try {
+    const last = await prisma.todo.findFirst({
+      where: { userId, dueOn: completeAt },
+      orderBy: { sortKey: "asc" },
+      select: { sortKey: true },
+    });
 
-  const sortKey = generateSortKey(last ? last.sortKey : null, null);
+    const sortKey = generateSortKey(last ? last.sortKey : null, null);
 
-  const todo = await prisma.todo.create({
-    data: {
-      userId,
-      title,
-      description,
-      priority,
-      dueOn: isAllDay ? completeAt : null,
-      dueAt: !isAllDay ? completeAt : null,
-      color: color ?? null,
-      sortKey,
-      isAllDay: isAllDay ?? null,
-    },
-  });
+    const todo = await prisma.todo.create({
+      data: {
+        userId,
+        title,
+        description,
+        priority,
+        dueOn: isAllDay ? completeAt : null,
+        dueAt: !isAllDay ? completeAt : null,
+        color: color ?? null,
+        sortKey,
+        isAllDay: isAllDay ?? null,
+      },
+    });
 
-  return res.status(201).json({ msg: "todo added sucessfully", todo });
+    return res.status(201).json({ msg: "todo added sucessfully", todo });
+  } catch {
+    console.error("Error while adding todo", error);
+    return res.status(500).json({
+      msg: "internal Server Error",
+    });
+  }
 });
 
 export default todoRouter;
