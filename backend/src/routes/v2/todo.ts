7882908f2f80f6eba42ca8c35a70d213b78c9669
Shiva Raghav <shivaraghav200701@gmail.com>
2026-04-11 18:46:37 -0500

    feat: added patch route for todos

--- backend/src/routes/v2/todo.ts
+++ backend/src/routes/v2/todo.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { CreateTodoSchema } from "@shiva200701/todotypes";
+import { CreateTodoSchema, UpdateTodoSchema } from "@shiva200701/todotypes";
 import { requireLogin } from "../../middleware.js";
 import { generateSortKey } from "../../utils/todo-ordering.js";
 import prisma from "../../db/index.js";
+import { Prisma } from "@prisma/client";
+import { log } from "console";
 
 const todoRouter = Router();
 
@@ -39,6 +41,13 @@ todoRouter.get("/", requireLogin, async (req, res) => {
 });
 
 todoRouter.post("/", requireLogin, async (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.status(401).json({
+      msg: "unauthorized",
+    });
+  }
+
   const { data, success, error } = CreateTodoSchema.safeParse(req.body);
 
   if (!success) {
@@ -48,26 +57,11 @@ todoRouter.post("/", requireLogin, async (req, res) => {
     });
   }
 
-  const userId = req.session.userId;
-  if (!userId) {
-    return res.status(401).json({
-      msg: "unauthorized",
-    });
-  }
-
-  const {
-    title,
-    description,
-    priority,
-    completeAt,
-    color,
-    reminder,
-    isAllDay,
-  } = data;
+  const { title, description, priority, due, color, reminder, isAllDay } = data;
 
   try {
     const last = await prisma.todo.findFirst({
-      where: { userId, dueOn: completeAt },
+      where: { userId, dueOn: due },
       orderBy: { sortKey: "asc" },
       select: { sortKey: true },
     });
@@ -78,10 +72,10 @@ todoRouter.post("/", requireLogin, async (req, res) => {
       data: {
         userId,
         title,
-        description,
+        description: description ?? null,
         priority,
-        dueOn: isAllDay ? completeAt : null,
-        dueAt: !isAllDay ? completeAt : null,
+        dueOn: isAllDay ? due : null,
+        dueAt: !isAllDay ? due : null,
         color: color ?? null,
         sortKey,
         isAllDay: isAllDay ?? null,
@@ -90,9 +84,86 @@ todoRouter.post("/", requireLogin, async (req, res) => {
 
     return res.status(201).json({
       msg: "todo added sucessfully",
-      todo: { ...todo, completeAt },
+      todo: { ...todo, due },
     });
   } catch {
+    console.error("Error while adding todo", error);
+    return res.status(500).json({
+      msg: "internal Server Error",
+    });
+  }
+});
+
+todoRouter.patch("/:id", requireLogin, async (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.status(401).json({
+      msg: "unauthorized",
+    });
+  }
+
+  const idParam = Array.isArray(req.params.id)
+    ? req.params.id[0]
+    : req.params.id;
+  if (!idParam) {
+    return res.status(400).json({
+      msg: "No todo id found in path",
+    });
+  }
+  try {
+    const existing = await prisma.todo.findFirst({
+      where: { id: parseInt(idParam), userId },
+    });
+
+    if (!existing) {
+      return res.status(404).json({ msg: "Todo not found" });
+    }
+
+    const { data, success, error } = UpdateTodoSchema.safeParse(req.body);
+
+    if (!success) {
+      return res.status(400).json({
+        msg: "Send proper data",
+        error,
+      });
+    }
+
+    const patch = data;
+    const updateData: Prisma.TodoUpdateInput = {};
+
+    //Todo move this to seperate function
+    if (patch.title !== undefined) updateData.title = patch.title;
+    if (patch.description !== undefined)
+      updateData.description = patch.description;
+    if (patch.priority !== undefined) updateData.priority = patch.priority;
+    if (patch.color !== undefined) updateData.color = patch.color;
+    if (patch.isAllDay !== undefined) updateData.isAllDay = patch.isAllDay;
+    if (patch.due !== undefined) {
+      if (patch.due === null) {
+        updateData.dueOn = null;
+        updateData.dueAt = null;
+      } else {
+        const allDay = patch.isAllDay ?? existing.isAllDay ?? true;
+        updateData.dueOn = allDay ? patch.due : null;
+        updateData.dueAt = allDay ? null : patch.due;
+        updateData.isAllDay = allDay;
+      }
+    }
+    if (patch.prevIndex !== undefined || patch.nextIndex !== undefined) {
+      updateData.sortKey = generateSortKey(
+        patch.prevIndex ?? null,
+        patch.nextIndex ?? null,
+      );
+    }
+    const updatedTodo = await prisma.todo.update({
+      where: { id: parseInt(idParam), userId },
+      data: updateData,
+    });
+
+    return res.status(200).json({
+      todo: updatedTodo,
+    });
+  } catch (error) {
     console.error("Error while adding todo", error);
     return res.status(500).json({
       msg: "internal Server Error",
