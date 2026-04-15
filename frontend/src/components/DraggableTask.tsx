af8c3f3c97ae6de3db59f467dc037dff6b47b0b6
Shiva Raghav <shivaraghav200701@gmail.com>
2026-04-15 13:32:13 -0500

    feat: added moreDropdown popover

--- frontend/src/components/DraggableTask.tsx
+++ frontend/src/components/DraggableTask.tsx
@@ -1,8 +1,11 @@
 import { cn } from "@/lib/utils";
 import type { TodoWithCompleteAtDateTime } from "@/types";
 import { useSortable } from "@dnd-kit/react/sortable";
-import { Check } from "lucide-react";
+import { Check, MoreHorizontal } from "lucide-react";
 import { useUpdateTodo } from "@/hooks/use-todos";
+import { Popover } from "./ui/popover";
+import { useState } from "react";
+import MoreOptionsDropDown from "./MoreOptionsDropDown";
 
 function DraggableTask({
   id,
@@ -17,6 +20,8 @@ function DraggableTask({
   column: string;
   className?: string;
 }) {
+  const [isMoreOptionsOpen, setIsMoreOptionsOpen] = useState(false);
+
   const { mutate } = useUpdateTodo();
   const { ref, isDragging } = useSortable({
     id,
@@ -40,19 +45,29 @@ function DraggableTask({
     <>
       <div
         className={cn(
-          "border border-border rounded-lg p-2.5 mb-2 w-[260px] min-h-[70px] bg-white shadow-2xs hover:shadow-card-hover hover:cursor-pointer",
+          "border border-border rounded-lg p-2.5 mb-2 w-[260px] min-h-[70px] bg-white shadow-2xs hover:shadow-card-hover hover:cursor-pointer relative group",
           className,
         )}
         ref={ref}
       >
+        <Popover
+          openPopover={isMoreOptionsOpen}
+          setOpenPopover={setIsMoreOptionsOpen}
+          content={<MoreOptionsDropDown />}
+          sideOffset={2}
+        >
+          <div className="absolute top-2 right-2 hover:bg-accent rounded-sm data-[state=open]:bg-accent hidden group-hover:block data-[state=open]:block">
+            <MoreHorizontal color="#808080" />
+          </div>
+        </Popover>
         <div className="flex gap-2">
           <button
-            className="border border-border rounded-full h-5 w-5 flex items-center justify-center group cursor-pointer"
+            className="border border-border rounded-full h-5 w-5 flex items-center justify-center group/check-mark cursor-pointer"
             onClick={() => {
               mutate({ id, data: { completed: !todo.completed } });
             }}
           >
-            <Check size={15} className="hidden group-hover:block" />
+            <Check size={15} className="hidden group-hover/check-mark:block" />
           </button>
           <div className="flex flex-col">
             <div className="text-md">{todo.title}</div>
---
+++ frontend/src/components/MoreOptionsDropDown.tsx
@@ -0,0 +1,5 @@
+function MoreOptionsDropDown() {
+  return <div>This is the popover content and it will be displayed here</div>;
+}
+
+export default MoreOptionsDropDown;
