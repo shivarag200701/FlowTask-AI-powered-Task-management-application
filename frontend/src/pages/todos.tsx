4897824d23bd1584814825251b75df5aa0340a2f
Shiva Raghav <shivaraghav200701@gmail.com>
2026-05-06 19:29:50 -0500

    feat: added url param clearing feature

--- frontend/src/pages/todos.tsx
+++ frontend/src/pages/todos.tsx
@@ -2,7 +2,6 @@ import EmptyState from "@/components/EmptyState";
 import { Button } from "@/components/ui/button";
 import { Kbd } from "@/components/ui/kbd";
 import TagFilterDisplayer from "@/features/tags/components/TagFilterDisplayer";
-import { useMediaQuery } from "@/hooks/use-media-query";
 import { useTags } from "@/hooks/use-tags";
 import PageContentHeader from "@/layouts/PageContentHeader";
 import PageWidthWrapper from "@/layouts/PageWidthWrapper";
@@ -15,8 +14,6 @@ function Todos() {
 
   const urlTags = searchParams.get("tagIds")?.split(",");
 
-  console.log(urlTags);
-
   const urlTagSet = new Set(urlTags);
 
   const { data: tags } = useTags({ query: { search: "" } });
@@ -27,6 +24,7 @@ function Todos() {
   useHotkeys("esc", () => {
     setSearchParams("");
   });
+  console.log("selectedTags", selectedTags);
 
   return (
     <div className={cn("")}>
@@ -35,18 +33,20 @@ function Todos() {
         controls={<div>This is the control</div>}
       />
       <PageWidthWrapper className="pt-6 px-3 lg:pt-12 flex flex-col overflow-x-auto ">
-        <div className="flex items-center justify-between gap-4 mb-4">
-          <TagFilterDisplayer tags={selectedTags} />
-          <button
-            className="h-[38px] flex gap-2 text-neutral-500 items-center transition-all duration-200 border border-transparent hover:border-border cursor-pointer px-3 py-2 rounded-md"
-            onClick={() => {
-              setSearchParams("");
-            }}
-          >
-            <div className="font-medium">Clear Filters</div>
-            <Kbd className="w-fit border">ESC</Kbd>
-          </button>
-        </div>
+        {urlTags && (
+          <div className="flex items-center justify-between gap-4 mb-4">
+            <TagFilterDisplayer tags={selectedTags} />
+            <button
+              className="h-[38px] flex gap-2 text-neutral-500 items-center transition-all duration-200 border border-transparent hover:border-border cursor-pointer px-3 py-2 rounded-md"
+              onClick={() => {
+                setSearchParams("");
+              }}
+            >
+              <div className="font-medium">Clear Filters</div>
+              <Kbd className="w-fit border">ESC</Kbd>
+            </button>
+          </div>
+        )}
         {(!searchParams.get("tagIds") ||
           searchParams.get("tagIds")?.length === 0) && (
           <EmptyState
