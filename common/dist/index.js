08b6a1a563cd32f13f91e2eca2ca180fb0ac76e0
Shiva Raghav <shivaraghav200701@gmail.com>
2026-06-09 16:03:38 -0500

    feat: removed predploy step and removed common folder

--- common/dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./v1.js";
-export * from "./v2/tag.js";
-export * from "./v2/todo.js";
-export * from "./v2/project.js";
-export * from "./utils/slug.js";
-//# sourceMappingURL=index.js.map
--- common/dist/utils/slug.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function createSlug(title: string, id: string): string;
-export declare function extractIdFromSlug(slug: string): string;
-//# sourceMappingURL=slug.d.ts.map
